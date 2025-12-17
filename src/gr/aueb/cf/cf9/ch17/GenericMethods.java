1c2a96f46f89f7cd85288a2c328ba4681c5b1eb2
sprs <spiroskos9@gmail.com>
2025-12-17 11:32:07 +0200

    feat :generic methods

--- src/gr/aueb/cf/cf9/ch17/GenericMethods.java
+++ src/gr/aueb/cf/cf9/ch17/GenericMethods.java
@@ -1,5 +1,7 @@
 package gr.aueb.cf.cf9.ch17;
 
+import java.util.List;
+
 public class GenericMethods {
 
     public static void main(String[] args) {
@@ -15,6 +17,26 @@ public class GenericMethods {
     public static <T> void  printArray(T[] array) {
         for (T element : array) {
             System.out.println(element);
+        }
+    }
+
+    public  static void print(GenericNode<?> node) {
+        System.out.println(node.getValue());
+    }
+
+    public  static void printNumbers(GenericNode<? extends Number>  node) {
+        System.out.println(node.getValue());
+    }
+
+
+    public  static void printNumbers2(GenericNode<? super Integer>  node) {
+        System.out.println(node.getValue());
+    }
+
+                                            //Consumer super - write       Producer extends - read
+    private static <T> void produceConsume(List<? super T > list, Iterable<? extends T> src) {
+        for (T item : src) {
+            list.add(item);
         }
     }
 }
