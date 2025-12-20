1d1e7513c25de9ea604776c47aaf74c622afce37
sprs <spiroskos9@gmail.com>
2025-12-20 14:45:29 +0200

    feat: update solutions ch14 Animal

--- src/gr/aueb/cf/cf9/solutions/ch14/Animal.java
+++ src/gr/aueb/cf/cf9/solutions/ch14/Animal.java
@@ -12,22 +12,6 @@ public class Animal {
         this.age = age;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
     public void speak(){
         System.out.println("My name is  " + name);
     }
